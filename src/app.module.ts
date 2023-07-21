873b5ecf38c3b20a9f535d904ed049b6dfc27078
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-07-21 01:37:26 -0300

    ✬ feat (database):  transactions model

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { AppHttpModule } from './app/http/http.module';
 import { AppClientModule } from '@src/client/client.module';
 import { AppConfigModule } from './config.module';
 import { AppLoggerModule } from './util/logger.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [AppConfigModule, AppLoggerModule, AppClientModule, AppHttpModule],
+  imports: [
+    AppConfigModule,
+    AppLoggerModule,
+    AppClientModule,
+    AppHttpModule,
+    MongooseModule.forRoot(process.env.DATABASE_URL),    
+  ],
+
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/models/transactions.ts
@@ -0,0 +1,47 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export interface ConversionRates {
+  [key: string]: number;
+}
+
+export type TransactionDocument = Transaction & Document;
+
+@Schema({
+  toJSON: {
+    transform: (_, ret): void => {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+    },
+  },
+})
+export class Transaction extends Document{
+  // @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  // user: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
+  transaction_id: string;
+
+  @Prop({ required: true })
+  from: string;
+
+  @Prop({ required: true })
+  value: number;
+
+  @Prop({ required: true })
+  to: string;
+
+  @Prop({ type: Object, required: true })
+  rates: ConversionRates;
+
+  @Prop({ type: Date, required: true })
+  date: Date;
+}
+
+export const TransactionSchema = SchemaFactory.createForClass(Transaction);
+
+export const Transactions = mongoose.model<TransactionDocument>(
+  'Transactions',
+  TransactionSchema,
+);
