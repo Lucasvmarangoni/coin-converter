e39c79375097777d4591b9cb6f407934b2539573
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-08-24 10:32:27 -0300

    ✬ test (unit): findOne user

---
+++ src/app/features/user/services/__test__/find.service.spec.ts
@@ -0,0 +1,49 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { TestingModule, Test } from '@nestjs/testing';
+import { FindUsersService, UserInfo } from '../find.service';
+
+describe('User create service', () => {
+  let findUsersService: FindUsersService;
+
+  const responseData = {
+    id: '64dfe5b66045b01ec3844b24',
+    name: 'teste',
+    username: '1teste',
+    email: 'johna@gmail.com',
+    password: '$2b$10$XdgOTKfhhgwCNdrcKXe3AeirZzrYugqmsNjQ.h3X1fU0EOnWOqvr.',
+    createdAt: new Date(),
+  };
+
+  const createTestingModuleWithData = async (
+    responseData?: UserInfo | undefined,
+  ) => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        FindUsersService,
+        {
+          provide: getModelToken('UserModel'),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(responseData),
+          },
+        },
+      ],
+    }).compile();
+
+    findUsersService = moduleRef.get<FindUsersService>(FindUsersService);
+  };
+
+  it('should be defined', async () => {
+    await createTestingModuleWithData();
+    expect(findUsersService).toBeDefined();
+  });
+
+  it('should call create with params and return user', async () => {
+    await createTestingModuleWithData(responseData);
+
+    const param = 'JohnDoe';
+    const response = await findUsersService.findOne(param);
+
+    expect(findUsersService).toBeInstanceOf(FindUsersService);
+    // expect(response).toEqual(responseData);
+  });
+});
