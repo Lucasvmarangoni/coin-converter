48270430ba0b67cc846e7615c2dc8ee553c8c0a5
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-08-24 10:01:52 -0300

    ✬ refactor: move file

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppUserModule } from './features/user/user.module';
-import { AppConverterModule } from './features/converter/converter.module';
+import { AppConverterModule } from './converter/converter.module';
+import { AppUserModule } from './user/user.module';
 
 @Module({
   imports: [AppConverterModule, AppUserModule],
