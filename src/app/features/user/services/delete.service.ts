649b74c2c723671c035ae6627589652c26557431
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-09-07 11:40:45 -0300

    ✬ style: changed nomenclatures, directories and others

--- src/app/features/user/services/delete.service.ts
+++ src/app/features/user/services/delete.service.ts
@@ -4,17 +4,17 @@ import { User } from '@src/app/models/user';
 import { Model } from 'mongoose';
 
 @Injectable()
-export class DeleteAllUsersService {
+export class DeleteService {
   constructor(
     @InjectModel('UserModel')
     private userModel: Model<User>,
   ) {}
 
-  async execute(): Promise<void> {
-    await this.userModel.deleteMany({});
+  async execute(user: User): Promise<void> {
+    await this.userModel.deleteOne({ id: user.id });
   }
 
-  isEmail(email: string) {
+  private isEmail(email: string) {
     const regex =
       /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
