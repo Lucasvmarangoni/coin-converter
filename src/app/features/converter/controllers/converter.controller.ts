1ccefe02b40a55777ceb50dbcbf8466341c3cc5a
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-09-07 07:17:18 -0300

    ✬ feat: changed custom exceptions handling to HttpException

--- src/app/features/converter/controllers/converter.controller.ts
+++ src/app/features/converter/controllers/converter.controller.ts
@@ -7,20 +7,17 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { BaseController } from '@src/index';
 import { Response } from 'express';
 import { ConverterService } from '../services/converter.service';
 import { FindAllService } from '../services/find-all.service';
 import { JwtAuthGuard } from '@src/app/auth/guards/jwt-auth.guard';
 
 @Controller('converter')
-export class ConverterController extends BaseController {
+export class ConverterController {
   constructor(
     private converterService: ConverterService,
     private findAllService: FindAllService,
-  ) {
-    super();
-  }
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Post(':to/:amount/:from?')
@@ -31,27 +28,19 @@ export class ConverterController extends BaseController {
   ): Promise<void> {
     const { from, to, amount } = params;
 
-    try {
-      const converter = await this.converterService.execute({
-        to,
-        amount: +amount,
-        from,
-        user: req.user.id,
-      });
-      res.status(201).json(converter);
-    } catch (err) {
-      this.sendCreateUpdateErrorResponse(res, err);
-    }
+    const converter = await this.converterService.execute({
+      to,
+      amount: +amount,
+      from,
+      user: req.user.id,
+    });
+    res.status(201).json(converter);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('all')
-  public async listAll(@Res() res: Response): Promise<void> {
-    try {
-      const listAll = await this.findAllService.execute();
-      res.status(200).json(listAll);
-    } catch (err) {
-      this.sendErrorResponse(res, err);
-    }
+  public async listAll(@Req() req, @Res() res): Promise<void> {
+    const listAll = await this.findAllService.execute(req.user.id);
+    res.status(200).json(listAll);
   }
 }
