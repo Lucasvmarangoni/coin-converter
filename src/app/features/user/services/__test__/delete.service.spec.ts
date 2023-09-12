03693d87e4e33aa5736bf396d5c6e63a51ef41b3
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-09-11 23:22:54 -0300

    ✬ test(unit): add delete service testing

---
+++ src/app/features/user/services/__test__/delete.service.spec.ts
@@ -0,0 +1,42 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from '@src/app/models/user';
+import { Model } from 'mongoose';
+import { DeleteService } from '../delete.service';
+
+describe('DeleteService', () => {
+  let service: DeleteService;
+  let userModel: Model<User>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        DeleteService,
+        {
+          provide: getModelToken('UserModel'),
+          useValue: {
+            deleteOne: jest.fn().mockResolvedValue(true),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<DeleteService>(DeleteService);
+    userModel = module.get<Model<User>>(getModelToken('UserModel'));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should delete a user', async () => {
+    const user = {
+      id: '1',
+      name: 'Test User',
+      email: 'test@example.com',
+    };
+    await service.execute(user as User);
+
+    expect(userModel.deleteOne).toHaveBeenCalledWith({ id: user.id });
+  });
+});
